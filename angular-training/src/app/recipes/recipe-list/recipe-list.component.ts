9f063a981b7acc95a6c67d0567b95cadebb53b10
Phu Nitro5 <hoducphu241@gmail.com>
2023-10-17 22:43:58 +0700

    Section3

--- angular-training/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-training/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[] =[
+    new Recipe('A test recipe','test description', 'https://cdn.nhathuoclongchau.com.vn/unsafe/800x0/filters:quality(95)/https://cms-prod.s3-sgn09.fptcloud.com/cach_lam_banh_pizza_thom_ngon_chuan_nha_hang_2_43d4f180fd.png'),
+    new Recipe('A test recipe','test description', 'https://cdn.nhathuoclongchau.com.vn/unsafe/800x0/filters:quality(95)/https://cms-prod.s3-sgn09.fptcloud.com/cach_lam_banh_pizza_thom_ngon_chuan_nha_hang_2_43d4f180fd.png')
+  ];
 
+  constructor() {
+
+  }
 }
